4c2dfc6d1ea642ff0a224ae52aad78c94e44ee80
Val Currie <contact@valcuri.email>
2021-04-22 11:29:27 -0500

    click event on walkers

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,13 +1,29 @@
 import { getWalkers } from "./database.js"
 
+// when you click a walker, display their service area
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("walker")) {
+            // extract the walker id from the id property of the list item
+            const [,walkerId] = itemClicked.id.split("--")
+
+            for (const walker of walkers) {
+                if (walker.id === parseInt(walkerId)) {
+                    window.alert(`${walker.name} services ${walker.city}`)
+                }
+            }
+        }
+    }
+)
+
 const walkers = getWalkers()
-
-
 export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.name}</li>`
+        walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`
     }
 
     walkerHTML += "</ul>"

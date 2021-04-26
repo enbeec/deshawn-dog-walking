735d6d21d49fe761b0a35fb7fcc16fdd4c24d945
Val Currie <contact@valcuri.email>
2021-04-26 14:46:12 -0500

    click event is working

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -7,10 +7,25 @@ const walkers = getWalkers()
 document.addEventListener(
     "click",
     clickEvent => {
-        // TODO: finish this click event
+        const itemClicked = clickEvent.target
         // if clicked a pet of id petId
-        // get that pet from pets
-        // find a walker with id === pet.walkerId
+        if (itemClicked.id.startsWith("pet")) {
+            let [,petId] = itemClicked.id.split("--")
+            // QUESTION: is this an abuse of dynamic typing?
+            petId = parseInt(petId)
+
+            // could also be:
+            // const pet = pets.find(onePet => onePet.id === petId)
+            const pet = pets.find(
+                onePet => {
+                    return onePet.id === petId
+                }
+            ) // but this is more readable for now
+            // okay I tried the implicit return way here :)
+            const walker = walkers.find(oneWalker => oneWalker.id === pet.walkerId)
+
+            window.alert(`${pet.name} is being walked by ${walker.name}`)
+        }
     }
 )
 

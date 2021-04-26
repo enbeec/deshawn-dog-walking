4f3195b49c486bf684e07bd8b2fa5e5a31a81d89
Val Currie <contact@valcuri.email>
2021-04-26 14:22:08 -0500

    getting ready to add click event

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,24 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
+// QUESTION: pro/con of importing data in top scope of module. Does `pets` just persist (not get GC'd) because I did it this way?
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    clickEvent => {
+        // TODO: finish this click event
+        // if clicked a pet of id petId
+        // get that pet from pets
+        // find a walker with id === pet.walkerId
+    }
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
